2d3ed9709e43e1ec22c3a697640ce2667b6f3c65
Enes Kızılca <76004060+eneskizilca@users.noreply.github.com>
2024-11-27 00:12:55 +0300

    Add files via upload

---
+++ CarRacingGame/CarRacingGame/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace CarRacingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.finish = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.car3 = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.finish.SuspendLayout();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(45, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 19);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(232, 112);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(143, 19);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(418, 112);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(143, 19);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(604, 112);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(143, 19);
+            this.panel4.TabIndex = 3;
+            // 
+            // finish
+            // 
+            this.finish.BackColor = System.Drawing.Color.Maroon;
+            this.finish.Controls.Add(this.label1);
+            this.finish.Location = new System.Drawing.Point(784, 0);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(142, 482);
+            this.finish.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(24, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FINISH";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(45, 264);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(143, 19);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.DimGray;
+            this.panel7.Controls.Add(this.btnStart);
+            this.panel7.Location = new System.Drawing.Point(0, 407);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(923, 74);
+            this.panel7.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
+            this.btnStart.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(327, 16);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(267, 41);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Race";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.White;
+            this.panel8.Location = new System.Drawing.Point(232, 264);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(143, 19);
+            this.panel8.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.White;
+            this.panel9.Location = new System.Drawing.Point(418, 264);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(143, 19);
+            this.panel9.TabIndex = 3;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.White;
+            this.panel10.Location = new System.Drawing.Point(604, 264);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(143, 19);
+            this.panel10.TabIndex = 4;
+            // 
+            // car3
+            // 
+            this.car3.BackColor = System.Drawing.Color.Transparent;
+            this.car3.Image = global::CarRacingGame.Properties.Resources.araba_3;
+            this.car3.Location = new System.Drawing.Point(45, 319);
+            this.car3.Name = "car3";
+            this.car3.Size = new System.Drawing.Size(49, 49);
+            this.car3.TabIndex = 9;
+            this.car3.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.Color.Transparent;
+            this.car2.Image = global::CarRacingGame.Properties.Resources.araba_2;
+            this.car2.Location = new System.Drawing.Point(45, 175);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(49, 49);
+            this.car2.TabIndex = 8;
+            this.car2.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.BackColor = System.Drawing.Color.Transparent;
+            this.car1.Image = global::CarRacingGame.Properties.Resources.araba_1;
+            this.car1.Location = new System.Drawing.Point(45, 33);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(49, 49);
+            this.car1.TabIndex = 7;
+            this.car1.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.Timer);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(920, 480);
+            this.Controls.Add(this.car3);
+            this.Controls.Add(this.car2);
+            this.Controls.Add(this.car1);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.finish);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Car Racing";
+            this.finish.ResumeLayout(false);
+            this.finish.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel finish;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox car3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ CarRacingGame/CarRacingGame/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRacingGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int carSpeed1;
+        int carSpeed2;
+        int carSpeed3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            car1.Left = 45;
+            car2.Left = 45;
+            car3.Left = 45;
+            timer.Start();
+        }
+
+        private void Timer(object sender, EventArgs e)
+        {
+            carSpeed1 = random.Next(5, 20);
+            carSpeed2 = random.Next(5, 20);
+            carSpeed3 = random.Next(5, 20);
+
+            car1.Left += carSpeed1;
+            car2.Left += carSpeed2;
+            car3.Left += carSpeed3;
+
+            if(car1.Right >= finish.Left || car2.Right >= finish.Left || car3.Right >= finish.Left)
+            {
+                timer.Stop();
+            }
+
+            if (car1.Right >= finish.Left)
+            {
+                MessageBox.Show("Car 1 Win!");
+            }
+            if (car2.Right >= finish.Left)
+            {
+                MessageBox.Show("Car 2 Win!");
+            }
+            if (car3.Right >= finish.Left)
+            {
+                MessageBox.Show("Car 3 Win!");
+            }
+        }
+    }
+}
